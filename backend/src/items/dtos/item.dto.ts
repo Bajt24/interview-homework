e64f9d0821bb655d50be6040f0cb9fc0d054713d
Marek Bajtalon <bajtcz@gmail.com>
2025-08-24 22:56:40 +0200

    feat(be): item entity

---
+++ backend/src/items/dtos/item.dto.ts
@@ -0,0 +1,29 @@
+import { IsInt, IsNotEmpty, IsNumber, IsOptional, IsPositive, IsString, IsUrl, Min } from "class-validator"
+import { PartialType } from '@nestjs/mapped-types';
+import { Transform } from "class-transformer";
+
+export class ItemDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @IsOptional()
+  @IsUrl()
+  imageUrl: string;
+
+  @IsOptional()
+  @IsInt()
+  @Min(0)
+  quantity?: number;
+
+  @IsNumber()
+  @IsPositive()
+  @Transform(({ value }) => +(+value).toFixed(2))
+  unitPrice: number;
+}
+
+export class UpdateItemDto extends PartialType(ItemDto) {}
---
+++ backend/src/items/entities/item.entity.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Item {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ nullable: true })
+  imageUrl: string;
+
+  @Column({ nullable: true})
+  description: string;
+
+  @Column({ type: 'int', default: 0 })
+  quantity: number;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  unitPrice: number;
+}
