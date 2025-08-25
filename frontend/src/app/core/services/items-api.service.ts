5824c5c307719cea1d971f703d384c5395de930a
Marek Bajtalon <bajtcz@gmail.com>
2025-08-25 21:30:06 +0200

    feat(fe): items api service

---
+++ frontend/src/app/core/services/items-api.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { CreateWarehouseItemDto, UpdateWarehouseItemDto, WarehouseItem } from '../models/warehouse-item.interface';
+
+@Injectable()
+export class ItemsApiService {
+  constructor(private http: HttpClient) { }
+
+  public getAll(): Observable<WarehouseItem[]> {
+    return this.http.get<WarehouseItem[]>('/items');
+  }
+
+  public create(item: CreateWarehouseItemDto): Observable<WarehouseItem> {
+    return this.http.post<WarehouseItem>('/items', item);
+  }
+
+  public update(id: number, item: UpdateWarehouseItemDto): Observable<WarehouseItem> {
+    return this.http.put<WarehouseItem>(`/items/${id}`, item);
+  }
+
+  public delete(id: number): Observable<void> {
+    return this.http.delete<void>(`/items/${id}`);
+  }
+}
