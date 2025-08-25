b58b7ff61b8bce68d735ecc2853e9b1e29a75eab
Marek Bajtalon <bajtcz@gmail.com>
2025-08-25 21:21:01 +0200

    feat(be): enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
       enableImplicitConversion: true,
     },
   }));
+  app.enableCors(); // for development/assignment purposes only
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
